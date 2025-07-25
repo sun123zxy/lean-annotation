d7dc9ca6795ec3738e85fc13f5fec04aa1cfabea
sun123zxy <304244450@qq.com>
2025-07-25 13:35:48 +0800

    upd

--- LeanAnnotation/6900.lean
+++ LeanAnnotation/6900.lean
@@ -9,7 +9,8 @@ Our strategy is as follows:
 
 - Show that $\varphi(p^t) = \varphi(2 p^t)$.
 
-- Show that the order of $r + p^t$ is divided by $\varphi(p^t) = \varphi(2 p^t)$. In fact,
+- Show that the order of $r + p^t$ modulo $2 p^t$ is divided by $\varphi(p^t) = \varphi(2 p^t)$.
+  In fact, take $\alpha$ be the order of $r + p^t$ modulo $2 p^t$, we have
   $$
   (r + p^t)^\alpha \equiv 1 \pmod{2 p^t}
   $$
@@ -32,7 +33,7 @@ Our strategy is as follows:
 
 open Nat
 
-theorem primitive_root (p : ℕ) (t : ℕ) (r : ℤ)
+theorem rpt_primitive_root (p : ℕ) (t : ℕ) (r : ℤ)
     (hp : Nat.Prime p)
     (hp_odd : Odd p)
     (ht : 0 < t)
@@ -42,25 +43,56 @@ theorem primitive_root (p : ℕ) (t : ℕ) (r : ℤ)
 
   haveI : Fact (Nat.Prime p) := ⟨hp⟩
 
-  haveI : IsUnit (r : ZMod (p ^ t)) := by sorry
-  obtain ⟨ur, h_ur⟩ := this
-  rw [← h_ur, orderOf_units] at hr_prim
-
-  haveI : IsUnit (r + p ^ t : ZMod (2 * p ^ t)) := by sorry
-  obtain ⟨urpt, h_urpt⟩ := this
-  rw [← h_urpt, orderOf_units]
-
-
   have totient_eq : φ (2 * p ^ t) = φ (p ^ t) := by
     sorry
 
+  haveI : IsUnit (r : ZMod (p ^ t)) := by
+    apply IsOfFinOrder.isUnit
+    rw [← orderOf_pos_iff]
+    rw [hr_prim]
+    exact pos_of_neZero (φ (p ^ t))
+  obtain ⟨ur, h_ur⟩ := this
+  rw [← h_ur, orderOf_units] at hr_prim
+
+  haveI : IsUnit (r + p ^ t : ZMod (2 * p ^ t)) := by
+    norm_cast
+    rw [ZMod.isUnit_iff_coprime]
+    rw [coprime_iff_gcd_eq_one]
+    lift p to PNat using pos_of_neZero p
+    norm_cast
+    rw [← PNat.gcd_coe]
+    #check PNat.gcd
+    #check PNat.Coprime.gcd_mul
+    rw [PNat.Coprime.gcd_mul]
+    -- show that gcd(p^t, r+p^t) = 1
+
+
+  obtain ⟨urpt, h_urpt⟩ := this
+  rw [← h_urpt, orderOf_units]
+
   -- Show the equality by divisibility
   apply Nat.dvd_antisymm
-  · rw [← ZMod.card_units_eq_totient]
+  · -- Direction 1: $\mathrm{ord}_{2p^t}(r + p^t) \mid \varphi(2p^t)$ by Lagrange's theorem
+    rw [← ZMod.card_units_eq_totient]
     exact orderOf_dvd_card
-  · rw [totient_eq]
+  · -- Direction 2: $\varphi(p^t) = \mathrm{ord}_{p^t}(r) \mid \mathrm{ord}_{2p^t}(r + p^t)$
+    rw [totient_eq]
     rw [← hr_prim]
-
-    -- Show that orderOf ur divides orderOf urpt
+    -- Use $(r + p^t)^{\mathrm{ord}_{2p^t}(r + p^t)} = 1$ in $\mathbb{Z}/2p^t\mathbb{Z}$
+    haveI := pow_orderOf_eq_one urpt
+    -- Reduce to modulo $p^t$
+    apply_fun (fun x => x.val.val % (p ^ t)) at this
+    simp [ZMod.val_one_eq_one_mod] at this
+    rw [h_urpt] at this
+    rw [← ZMod.natCast_eq_natCast_iff'] at this
+    simp only [ZMod.natCast_val, dvd_mul_left, ZMod.cast_pow, ZMod.cast_add, ZMod.cast_intCast,
+      ZMod.cast_natCast, cast_one] at this
+    -- use $p^t \equiv 0 \pmod{p^t}$ to simplify
+    conv at this => lhs; lhs; rhs; tactic => norm_cast
+    simp only [CharP.cast_eq_zero, add_zero] at this
+    -- Therefore $\mathrm{ord}_{p^t}(r) \mid \mathrm{ord}_{2p^t}(r + p^t)$
+    rw [← h_ur] at this
     rw [orderOf_dvd_iff_pow_eq_one]
-    sorry
+    apply_fun Units.val
+    · simp [this]
+    · exact Units.val_injective
